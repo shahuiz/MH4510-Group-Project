a92bdfb570616da01fc2bd6459fca09ea868e44c
BB1101 <91317427+BB1101@users.noreply.github.com>
2021-09-28 15:45:07 +0800

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,3 +1,87 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from imutils import paths
+import numpy as np
+import argparse
+import imutils
+import cv2
+import os
+
+$ pip install imutils
+
+def image_to_feature_vector(image, size=(32, 32)):
+	# resize the image to a fixed size, then flatten the image into
+	# a list of raw pixel intensities
+	return cv2.resize(image, size).flatten()
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset")
+ap.add_argument("-k", "--neighbors", type=int, default=1,
+	help="# of nearest neighbors for classification")
+ap.add_argument("-j", "--jobs", type=int, default=-1,
+	help="# of jobs for k-NN distance (-1 uses all available cores)")
+args = vars(ap.parse_args())
+
+# grab the list of images that we'll be describing
+print("[INFO] describing images...")
+imagePaths = list(paths.list_images(args["dataset"]))
+# initialize the raw pixel intensities matrix, the features matrix,
+# and labels list
+rawImages = []
+features = []
+labels = []
+
+for (i, imagePath) in enumerate(imagePaths):
+	# load the image and extract the class label (assuming that our
+	# path as the format: /path/to/dataset/{class}.{image_num}.jpg
+	image = cv2.imread(imagePath)
+	label = imagePath.split(os.path.sep)[-1].split(".")[0]
+	# extract raw pixel intensity "features", followed by a color
+	# histogram to characterize the color distribution of the pixels
+	# in the image
+	pixels = image_to_feature_vector(image)
+	hist = extract_color_histogram(image)
+	# update the raw images, features, and labels matricies,
+	# respectively
+	rawImages.append(pixels)
+	features.append(hist)
+	labels.append(label)
+	# show an update every 1,000 images
+	if i > 0 and i % 1000 == 0:
+		print("[INFO] processed {}/{}".format(i, len(imagePaths)))
+
+# show some information on the memory consumed by the raw images
+# matrix and features matrix
+rawImages = np.array(rawImages)
+features = np.array(features)
+labels = np.array(labels)
+print("[INFO] pixels matrix: {:.2f}MB".format(
+	rawImages.nbytes / (1024 * 1000.0)))
+print("[INFO] features matrix: {:.2f}MB".format(
+	features.nbytes / (1024 * 1000.0)))
+
+# partition the data into training and testing splits, using 75%
+# of the data for training and the remaining 25% for testing
+(trainRI, testRI, trainRL, testRL) = train_test_split(
+	rawImages, labels, test_size=0.25, random_state=42)
+(trainFeat, testFeat, trainLabels, testLabels) = train_test_split(
+	features, labels, test_size=0.25, random_state=42)
+
+# train and evaluate a k-NN classifer on the raw pixel intensities
+print("[INFO] evaluating raw pixel accuracy...")
+model = KNeighborsClassifier(n_neighbors=args["neighbors"],
+	n_jobs=args["jobs"])
+model.fit(trainRI, trainRL)
+acc = model.score(testRI, testRL)
+print("[INFO] raw pixel accuracy: {:.2f}%".format(acc * 100))
+
+
+
+
+
+
 from app import app
 from db_config import mysql
 from operator import itemgetter
