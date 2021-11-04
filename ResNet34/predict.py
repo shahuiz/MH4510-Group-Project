081cfeb9b609388b91a61bc3da7f3b6bf9333560
yuhan <80869892+yuhan0205@users.noreply.github.com>
2021-11-04 19:39:34 +0800

    Update predict.py

--- ResNet34/predict.py
+++ ResNet34/predict.py
@@ -28,30 +28,24 @@ def main():
 def read_data(root: str):
     assert os.path.exists(root), "dataset root: {} does not exist.".format(root)
 
-    # 遍历文件夹，一个文件夹对应一个类别
     tumor_class = [cla for cla in os.listdir(root) if os.path.isdir(os.path.join(root, cla))]
-    # 排序，保证顺序一致
     tumor_class.sort()
-    # 生成类别名称以及对应的数字索引
     class_indices = dict((k, v) for v, k in enumerate(tumor_class))
     json_str = json.dumps(dict((val, key) for key, val in class_indices.items()), indent=4)
     with open('class_indices.json', 'w') as json_file:
         json_file.write(json_str)
 
-    test_images_path = []  # 存储训练集的所有图片路径
-    test_images_label = []  # 存储训练集图片对应索引信息
+    test_images_path = []  
+    test_images_label = []  
 
-    every_class_num = []  # 存储每个类别的样本总数
-    supported = [".jpg", ".JPG", ".png", ".PNG"]  # 支持的文件后缀类型
-    # 遍历每个文件夹下的文件
+    every_class_num = []  
+    supported = [".jpg", ".JPG", ".png", ".PNG"]  
+    
     for cla in tumor_class:
         cla_path = os.path.join(root, cla)
-        # 遍历获取supported支持的所有文件路径
         images = [os.path.join(root, cla, i) for i in os.listdir(cla_path)
                   if os.path.splitext(i)[-1] in supported]
-        # 获取该类别对应的索引
         image_class = class_indices[cla]
-        # 记录该类别的样本数量
         every_class_num.append(len(images))
 
         for img_path in images:
@@ -63,18 +57,12 @@ def read_data(root: str):
 
     plot_image = False
     if plot_image:
-        # 绘制每种类别个数柱状图
         plt.bar(range(len(tumor_class)), every_class_num, align='center')
-        # 将横坐标0,1,2,3,4替换为相应的类别名称
         plt.xticks(range(len(tumor_class)), tumor_class)
-        # 在柱状图上添加数值标签
         for i, v in enumerate(every_class_num):
             plt.text(x=i, y=v + 5, s=str(v), ha='center')
-        # 设置x坐标
         plt.xlabel('image class')
-        # 设置y坐标
         plt.ylabel('number of images')
-        # 设置柱状图的标题
         plt.title('tumor class distribution')
         plt.show()
 
@@ -122,8 +110,7 @@ def resnet34_classifier(img_path: str, img_label: str, iter: int):
         output = torch.squeeze(model(img.to(device))).cpu()
         predict = torch.softmax(output, dim=0)
         predict_cla = torch.argmax(predict).numpy()
-
-    # print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)], predict[predict_cla].numpy())
+        
     print_res = "iter: {}   label: {}   pred.class: {}   prob: {:.3}".format(iter,
                                                                              class_indict[str(img_label)],
                                                                              class_indict[str(predict_cla)],
@@ -131,7 +118,6 @@ def resnet34_classifier(img_path: str, img_label: str, iter: int):
     plt.title(print_res)
     print(print_res)
 
-    # plt.show()
     return class_indict[str(img_label)], class_indict[str(predict_cla)]
 
 
