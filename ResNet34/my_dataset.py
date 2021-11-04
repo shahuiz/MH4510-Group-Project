555a9cd9a721be0edd5d76a88d81346b892bd820
yuhan <80869892+yuhan0205@users.noreply.github.com>
2021-11-04 19:41:35 +0800

    Update my_dataset.py

--- ResNet34/my_dataset.py
+++ ResNet34/my_dataset.py
@@ -5,7 +5,6 @@ from torch.utils.data import Dataset
 
 
 class MyDataSet(Dataset):
-    """自定义数据集"""
 
     def __init__(self, images_path: list, images_class: list, transform=None):
         self.images_path = images_path
@@ -19,7 +18,6 @@ class MyDataSet(Dataset):
             ratio = w / h
             if ratio > 10 or ratio < 0.1:
                 delete_img.append(index)
-                # print(img_path, ratio)
 
         for index in delete_img[::-1]:
             self.images_path.pop(index)
@@ -30,7 +28,6 @@ class MyDataSet(Dataset):
 
     def __getitem__(self, item):
         img = Image.open(self.images_path[item])
-        # RGB为彩色图片，L为灰度图片
         if img.mode != 'RGB':
             raise ValueError("image: {} isn't RGB mode.".format(self.images_path[item]))
         label = self.images_class[item]
@@ -42,7 +39,6 @@ class MyDataSet(Dataset):
 
     @staticmethod
     def collate_fn(batch):
-        # 官方实现的default_collate可以参考
         # https://github.com/pytorch/pytorch/blob/67b7e751e6b5931a9f45274653f4f653a4e6cdf6/torch/utils/data/_utils/collate.py
         images, labels = tuple(zip(*batch))
 
